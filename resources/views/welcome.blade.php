2d064816283e90ac9e408726cf2550ed8198057a
유형열 <>
2019-01-21 15:36:23 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,17 +41,15 @@
                 <div>
                 {!! trans('auth.failed') !!}
                 </div>
-                <div>
                 <div class="navbar-header">
                     <div class="dropdown-toggle-wrap">
                         <div class="dropdown navbar-toggle langtoggle" >
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{Cookie::get('language') ? Cookie::get('language') : 'en'}}</a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{Cookie::get('language') ? Cookie::get('language') : 'en'}}</a>
                             <ul class="dropdown-menu langbg mainlang ">
                                 <li><a href="/lang/en">en</li>
                                 <li><a href="/lang/kr">kr</li>
                             </ul>
                         </div>
-                        
                     </div>
                 </div>
                 <div class="title">Laravel</div>
