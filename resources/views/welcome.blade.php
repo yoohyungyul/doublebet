28815595bcc71308807060af836fff5f95962b10
유형열 <>
2019-01-21 17:15:08 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,12 +34,28 @@
                 font-size: 96px;
             }
         </style>
+
+        <script type="text/javascript">
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement(
+                    {
+                        pageLanguage: 'ko',
+                        includedLanguages: 'en,es,id,ja,zh-CN,zh-TW',
+                        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+                    },'google_translate_element');
+            }
+
+
+        </script>
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
     </head>
     <body>
 
     <div id="google_translate_element"><div class="skiptranslate goog-te-gadget" dir="ltr" style=""><div id=":0.targetLanguage" class="goog-te-gadget-simple" style="white-space: nowrap;"><img src="https://www.google.com/images/cleardot.gif" class="goog-te-gadget-icon" alt="" style="background-image: url(&quot;https://translate.googleapis.com/translate_static/img/te_ctrl3.gif&quot;); background-position: -65px 0px;"><span style="vertical-align: middle;"><a aria-haspopup="true" class="goog-te-menu-value" href="javascript:void(0)"><span>언어 선택</span><img src="https://www.google.com/images/cleardot.gif" alt="" width="1" height="1"><span style="border-left: 1px solid rgb(187, 187, 187);">​</span><img src="https://www.google.com/images/cleardot.gif" alt="" width="1" height="1"><span aria-hidden="true" style="color: rgb(118, 118, 118);">▼</span></a></span></div></div></div>
 
-    
+
         <div class="container">
             <div class="content">
                
