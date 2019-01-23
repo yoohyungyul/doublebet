ede2e31240addd73e6955d87266a7b77462a76f1
유형열 <>
2019-01-23 15:23:33 +0900

    no message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Agent;
 
 class HomeController extends Controller
 {
@@ -24,7 +25,7 @@ class HomeController extends Controller
 
         echo Agent::isMobile();
 
-        
+
         return view('welcome');
 
     }
