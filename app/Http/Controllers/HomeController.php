c65ee2b3703c3a525a79656f365b2436ffc33be8
유형열 <>
2019-01-23 15:21:09 +0900

    no message

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+   
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return view('welcome');
+
+    }
+
+
+}
