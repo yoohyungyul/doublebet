a9c10648e0ec193a36b0bd1475a5ffde595e267a
유형열 <>
2019-02-13 12:18:14 +0900

    no message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Agent;
+use App\Subscribe;
 
 class HomeController extends Controller
 {
@@ -46,7 +48,30 @@ class HomeController extends Controller
 
     public function postSubscribe(Request $request) {
 
-        return $request->email;
+        // return $request->email;
+
+        try {
+            DB::beginTransaction();
+
+            // 거래 내역 등록
+            $subscribe = new Subscribe;
+            $subscribe->email = $request->email;
+            $subscribe->push();
+         
+
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            return "0";
+
+           
+        } finally {
+            DB::commit();
+        }
+
+        return "1";
+
+
     }
 
 }
