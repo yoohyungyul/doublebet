abd73bb603b276f149e9d1d85bfe0ed69fed2c46
유형열 <>
2019-02-18 17:59:39 +0900

    no message

--- resources/views/welcome_mobile.blade.php
+++ resources/views/welcome_mobile.blade.php
@@ -21,7 +21,7 @@ if(Cookie::get('language') == "kr") $paper_link = "https://bit.ly/2UUIkqs";
 <div id="pgcontainer">
     <header>
         <div id="navbar">
-			<a href="/" class="menubtn">menu</a>
+			<a href="#" class="menubtn">menu</a>
             <span class="newLogo"><img src="/images/mobile/logo.png" alt="Doublbet"></span>
 			<div class="top_sns">
 				<a href="javascript:alert('coming soon');"><img src="/images/mobile/icon_tel.png" alt=""></a>
