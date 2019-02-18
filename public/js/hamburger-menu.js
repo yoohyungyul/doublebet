40c359d02190078ccda8d92a2935adabccc9c46f
유형열 <>
2019-02-18 17:25:29 +0900

    no message

---
+++ public/js/hamburger-menu.js
@@ -0,0 +1,55 @@
+$(function(){
+	var menuwidth  = 280; // pixel value for sliding menu width
+	var menuspeed  = 300; // milliseconds for sliding menu animation time
+	var $bdy       = $('body');
+	var $container = $('#pgcontainer');
+	var $burger    = $('#hamburgermenu');
+	var negwidth   = "-"+menuwidth+"px";
+	var poswidth   = menuwidth+"px";
+
+	$('.menubtn').on('click',function(e){
+		if($bdy.hasClass('openmenu')) {
+			jsAnimateMenu('close');
+		} else {
+			jsAnimateMenu('open');
+		}
+		$('.menu_bi').show();
+		$('.menu_close').show();
+	});
+
+	$('.overlay').on('click', function(e){
+		if($bdy.hasClass('openmenu')) {
+			jsAnimateMenu('close');
+		}
+	});
+
+	$('a[href$="#"]').on('click', function(e){
+		e.preventDefault();
+	});
+
+	$('.menu_close').on('click',function(e){
+		 $(this).hide();
+		 $('.menu_bi').hide();
+		 if($bdy.hasClass('openmenu')) {
+			jsAnimateMenu('close');
+		}
+	});        
+  
+	function jsAnimateMenu(tog) {
+		if(tog == 'open') {
+			$bdy.addClass('openmenu');
+			$('.menubtn').addClass('close')				
+			$container.animate({marginRight: negwidth, marginLeft: poswidth}, menuspeed);
+			$burger.animate({width: poswidth}, menuspeed);
+			$('.overlay').animate({left: poswidth}, menuspeed);
+		}
+		
+		if(tog == 'close') {
+			$bdy.removeClass('openmenu');
+			$('.menubtn').removeClass('close')
+			$container.animate({marginRight: "0", marginLeft: "0"}, menuspeed);
+			$burger.animate({width: "0"}, menuspeed);
+			$('.overlay').animate({left: "0"}, menuspeed);
+		}
+	}  
+});
