0a7f1a59bfda3331619b61ad1f0b7b18688ca664
유형열 <>
2019-02-19 10:54:07 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@ if(Cookie::get('language') == "kr") $paper_link = "https://bit.ly/2UUIkqs";
 				<ul class="nav navbar-nav navbar-right">
 					 <li>
 						<div class="custom-select" style="width:60px;margin-top:14px;margin-right:16px;">
-                            <select  >
+                            <select oncchange="alert('dddd')" >
                                 <option value="/lang/en" @if(Cookie::get('language') == "en") selected @endif>EN</option>
                                 <option value="/lang/kr" @if(Cookie::get('language') == "kr") selected @endif>KR</option>
                             </select>
