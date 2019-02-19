059f84167b6dac939704779fdd2b4d8c66f449ad
유형열 <>
2019-02-19 14:12:02 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -905,37 +905,47 @@ if(Cookie::get('language') == "kr") $paper_link = "https://bit.ly/2UUIkqs";
 					</div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-4 text-center">
-                        <div class="team-thumb wow fadeInUp text-center" data-wow-delay="0.2s">
-                            <div class="">
-                                <p class="t_center"><img src="/images/team_01.jpg"  alt=""></p>
-                                <p class=" txt_type_02 txt_b">Kim Jae Hong</p>
+                    <div class="col-xs-4">
+                        <div class="team-thumb wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="team-thumb-box">
+                                <p class="t_center"><img src="/images/team_01.jpg" class="img-responsive" alt=""></p>
+                                <p class="team_name txt_type_02 txt_b">Kim Jae Hong</p>
                                 <p class="team_company txt_type_01">Co-founder & CEO of CHAINPLUS<br>CEO of Melonbit</p>
-                                <p class="team_link_01">&nbsp;<a href="https://bit.ly/2QI1b5H" target="_blank"><img src="/images/icon_link.jpg" width="22" height="22" alt=""></a></p>
+                                <p class="team_link_01"><a href="https://bit.ly/2QI1b5H" target="_blank"><img src="/images/icon_link.jpg" width="22" height="22" alt=""></a></p>
+                            </div>
+                            <div class="team-hover">
+                                <div class="team-item">&nbsp;</div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-xs-4 text-center">2
-                        <!-- <div class="team-thumb wow fadeInUp text-center" data-wow-delay="0.3s">
+                    <div class="col-xs-4">
+                        <div class="team-thumb wow fadeInUp" data-wow-delay="0.3s">
                             <div class="team-thumb-box ">
                                 <p class="t_center"><img src="/images/team_02.jpg" class="img-responsive" alt=""></p>
                                 <p class="team_name txt_type_02 txt_b">Hyung Yul Yoo</p>
                                 <p class="team_company txt_type_01">Co-founder & CTO of CHAINPLUS<br>CTO of Melonbit</p>
-                                
+                                <!-- <p class="team_link_02"><a href="https://bit.ly/2Spjz93" target="_blank"><img src="/images/icon_link.jpg" width="22" height="22" alt=""></a></p> -->
                             </div>
-                        </div> -->
+                            <div class="team-hover">
+                                <div class="team-item">&nbsp;</div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-xs-4 text-center">3
-                        <!-- <div class="team-thumb wow fadeInUp text-center" data-wow-delay="0.4s">
+                    <div class="col-xs-4">
+                        <div class="team-thumb wow fadeInUp" data-wow-delay="0.4s">
                             <div class="team-thumb-box ">
                                 <p class="t_center"><img src="/images/team_03.jpg" class="img-responsive" alt=""></p>
                                 <p class="team_name txt_type_02 txt_b">Sang Guk Lee</p>
                                 <p class="team_company txt_type_01">Co-founder & COO of CHAINPLUS<br>Director of MGAME</p>
+                                <!-- <p class="team_link_03"><a href="#"><img src="/images/icon_link.jpg" width="22" height="22" alt=""></a></p> -->
                             </div>
-                        </div> -->
+                            <div class="team-hover">
+                                <div class="team-item">&nbsp;</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <!-- <div class="row">
+                <div class="row">
                     <div class="col-xs-4">
                         <div class="team-thumb wow fadeInUp" data-wow-delay="0.5s">
                             <div class="team-thumb-box">
@@ -996,14 +1006,13 @@ if(Cookie::get('language') == "kr") $paper_link = "https://bit.ly/2UUIkqs";
                             <div class="team-thumb-box team_mg_02">
                                 <p class="t_center"><img src="/images/team_06.jpg" class="img-responsive" alt=""></p>
                                 <p class="team_name txt_type_02 txt_b">Yu Sik Kim</p>
-                                <p class="team_company txt_type_01">Founder & CEO of DCInside</p>
-                             </div>
+                                <p class="team_company txt_type_01">Founder & CEO of DCInside</p> </div>
                             <div class="team-hover">
                                 <div class="team-item">&nbsp;</div>
                             </div>
                         </div>
                     </div>
-                </div> -->
+                </div>
 				<!-- /team -->
 				<div class="col-xs-12 col-xs-12">
 					<p class="space_100">&nbsp;</p>
