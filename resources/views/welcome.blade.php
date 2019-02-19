c61c0bdf1b031d777a0c6bbade2f31250be65d0c
유형열 <>
2019-02-19 14:07:43 +0900

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -906,9 +906,9 @@ if(Cookie::get('language') == "kr") $paper_link = "https://bit.ly/2UUIkqs";
                 </div>
                 <div class="row">
                     <div class="col-xs-4 text-center">
-                        <div class=" wow fadeInUp text-center" data-wow-delay="0.2s">
-                            <div class="">
-                                <p class="t_center"><img src="/images/team_01.jpg" class="img-responsive" alt=""></p>
+                        <div class="team-thumb wow fadeInUp text-center" data-wow-delay="0.2s">
+                            <div class="team-thumb-box">
+                                <p class="t_center"><img src="/images/team_01.jpg"  alt=""></p>
                                 <p class="team_name txt_type_02 txt_b">Kim Jae Hong</p>
                                 <p class="team_company txt_type_01">Co-founder & CEO of CHAINPLUS<br>CEO of Melonbit</p>
                                 <p class="team_link_01"><a href="https://bit.ly/2QI1b5H" target="_blank"><img src="/images/icon_link.jpg" width="22" height="22" alt=""></a></p>
